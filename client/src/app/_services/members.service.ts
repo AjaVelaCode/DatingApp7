1ffca573f7cb24966af52c9c9b7437cf4a27f7df
Andrija Veljkovic <a.veljkovic@sks365.com>
2023-01-26 14:56:12 +0100

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -10,14 +10,13 @@ import { Member } from '../_models/member';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
-  testMem: any;
 
   constructor(private httpClient: HttpClient) {    
   }
 
   getMembers(){
     if (this.members.length > 0) return of(this.members); 
-    return this.httpClient.get<Member[]>(this.baseUrl + 'users').pipe(
+    return this.httpClient.get<Member[]>(`${this.baseUrl}users/`).pipe(
       map(members => {
         this.members = members;
         return members;
@@ -28,16 +27,24 @@ export class MembersService {
   getMember(username: string){
     const member =  this.members.find(x=> x.userName ===username);
     if (member) return of(member);
-    return this.httpClient.get<Member>(this.baseUrl + 'users/' + username);
+    return this.httpClient.get<Member>(`${this.baseUrl}users/` + username);
   }
 
   updateMember(member: Member){
-    return this.httpClient.put(this.baseUrl + 'users', member).pipe(
+    return this.httpClient.put(`${this.baseUrl}users`, member).pipe(
       map(()=>{
         const index = this.members.indexOf(member);
         this.members[index] = {...this.members[index], ...member}
       })
     )
+  }
+
+  setMainPhoto(photoId: number){
+    return this.httpClient.put(`${this.baseUrl}users/set-main-photo/${photoId}`, {});
+  }
+
+  deletePhoto(photoId: number){
+    return this.httpClient.delete(`${this.baseUrl}users/delete-photo/${photoId}`);
   }
 
 }
